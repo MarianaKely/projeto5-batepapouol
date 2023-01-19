861efa578b9e6359df1f2ff52890d0add7a724d2
MarianaKely <marianakely.r@gmail.com>
2023-01-19 16:33:17 -0300

    API implementation

---
+++ script.js
@@ -0,0 +1,5 @@
+
+const apilogin = "https://mock-api.driven.com.br/api/v6/uol/participants";
+const apimessage = "https://mock-api.driven.com.br/api/v6/uol/messages";
+const apistatus = "https://mock-api.driven.com.br/api/v6/uol/status";
+const apimembers = "https://mock-api.driven.com.br/api/v6/uol/participants";
